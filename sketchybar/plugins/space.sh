91fdcfd9071847bbc64b69f3eb425abc80595abc
Tomas Aleixo Ramos <tomas.ramosv21@gmail.com>
2024-06-08 12:37:58 +0800

    SketchyBar active space color change

--- sketchybar/plugins/space.sh
+++ sketchybar/plugins/space.sh
@@ -5,7 +5,7 @@
 # https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
 
 if [ "$SELECTED" = "true" ]; then
-  sketchybar -m --set "$NAME" icon.highlight=on icon.font="Iosevka Term:Heavy:17.0" icon.highlight_color=0xffCE8954
+  sketchybar -m --set "$NAME" icon.highlight=on icon.font="Iosevka Term:Heavy:17.0" icon.highlight_color=0xffecbe7b
 else
   sketchybar -m --set "$NAME" icon.highlight=on icon.font="Iosevka Term:SemiBold:17.0" icon.highlight_color=0xffbbc2cf
 fi
